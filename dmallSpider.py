b0415435f6a0950aea490349f2b81fee715972ed
chen.chen <chen.chen@dmall.com>
2018-03-30 14:00:37 +0800

    done

--- dmallSpider.py
+++ dmallSpider.py
@@ -24,44 +24,50 @@ def urlReq(url):
     return link_list
 
 def child_urlReq(c_url):
-    res=page_info(c_url,1)
-    print(res)
-    # movie_list=[]
-    # for i in result:
-    #     year=i['year']
-    #     title=i['title']
-    #     murl=i['url']
-    #     movie_list.append([title,year,murl])
-    # return
+    movie_dict=res_info(c_url)
+    return movie_dict
 
-def page_info(child_url,page_num):
-    r = urllib.request.urlopen(child_url).read()
-    # r.encoding='utf-8'
-    c_soup = BeautifulSoup(r, "lxml")
-    c_soup_h = c_soup.find_all("script")
-    x = str(c_soup_h[3]).split('\n')
-    y1, y2 = x[2], x[3]
-    movie_tag = y1.lstrip('var TYPE = \'').rstrip('\';')
-    LOC_ID = y2.lstrip('var LOC_ID = \'').rstrip('\';')
-    t = time.time()
-    temp_i=1
-    while temp_i<=page_num:
-        resource_url = "https://m.douban.com/rexxar/api/v2/subject_collection/" + movie_tag + "/items?os=ios&for_mobile=1&callback=jsonp" + str(page_num) + "&start=0&count=18&loc_id=" + LOC_ID + "&_=" + str(int(round(t * 1000)))
-        header = {
-            "Acceptl": "*/*",
-            "Referer": child_url,
-            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
-        }
-        r2 = requests.get(resource_url, headers=header)
-        print(r2, type(r2.text))
-        jsonp = r2.text.lstrip(';jsonp1(').rstrip(');')
-        j = json.loads(jsonp)
-        result = j['subject_collection_items']
-        total_num = j["total"]
-        page_num = math.ceil(total_num / 18)
-        print(total_num, page_num)
-
+def res_pagenum(c_url):
+    temp_j=trans_url(c_url,18)
+    total_num=temp_j["total"]
+    page_num=math.ceil(total_num/18)
+    print(total_num,page_num)
     return page_num
+
+def res_info(c_url):
+    temp_p=res_pagenum(c_url)
+    temp_j=trans_url(c_url,temp_p)
+    result=temp_j['subject_collection_items']
+    movie_list=[]
+    for i in result:
+        year=i['year']
+        title=i['title']
+        murl=i['url']
+        movie_list.append([title,year,murl])
+    return movie_list
+
+def trans_url(c_url,c_pageno):
+    r = urllib.request.urlopen(c_url).read()
+    # r.encoding='utf-8'
+    c_soup=BeautifulSoup(r, "lxml")
+    c_soup_h=c_soup.find_all("script")
+    x=str(c_soup_h[3]).split('\n')
+    y1,y2=x[2],x[3]
+    movie_tag=y1.lstrip('var TYPE = \'').rstrip('\';')
+    LOC_ID=y2.lstrip('var LOC_ID = \'').rstrip('\';')
+    t=time.time()
+    page_num=1
+    resource_url="https://m.douban.com/rexxar/api/v2/subject_collection/"+movie_tag+"/items?os=ios&for_mobile=1&callback=jsonp"+str(page_num)+"&start=0&count="+str(c_pageno)+"&loc_id="+LOC_ID+"&_="+str(int(round(t * 1000)))
+    header={
+        "Acceptl":"*/*",
+        "Referer":c_url,
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
+    }
+    r2=requests.get(resource_url,headers=header)
+    temp_str=";jsonp"+str(page_num)+"("
+    jsonp=r2.text.lstrip(temp_str).rstrip(');')
+    jsonpx=json.loads(jsonp)
+    return jsonpx
 
 if __name__=='__main__':
     requestUrl="https://m.douban.com/movie"
